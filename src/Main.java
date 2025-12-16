a9bc04c9312f782d34b32d1a7750f9b8dda29900
Super User <root@localhost.localdomain>
2025-12-16 14:47:42 -0300

    teste2

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
     static class MeuHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            String response = "<h1>Java Web sem Maven e sem Tomcat!</h1>";
+            String response = "<h1>teste</h1>";
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
