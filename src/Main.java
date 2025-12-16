3741eae3c31ffa416dd0eab3fce70db0dd5353b5
Super User <root@localhost.localdomain>
2025-12-16 14:26:29 -0300

    teste1

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", new MeuHandler());
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Servidor rodando em http://localhost:8080");
+    }
+
+    static class MeuHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "<h1>Java Web sem Maven e sem Tomcat!</h1>";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
+
